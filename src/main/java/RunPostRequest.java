0ae94123f73682f2a912ec384cd947e6f8a677ee
Serhat <serhat.ozdursun@gmail.com>
2022-11-02 15:14:00 +0200

    update karate

--- src/main/java/RunPostRequest.java
+++
@@ -1,8 +0,0 @@
-import com.intuit.karate.KarateOptions;
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
-
-@RunWith(Karate.class)
-@KarateOptions(features = "src/features", tags = "@POST")
-public class RunPostRequest {
-}
